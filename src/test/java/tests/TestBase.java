9bb522d17eba77e58b8b36785ae495783f3ff1d9
VadimTeslyuk <eryilyvatar@gmail.com>
2024-09-03 22:27:18 +0300

    first step

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,15 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+}
